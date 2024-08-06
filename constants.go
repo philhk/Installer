bbf057fc4f373ed61ffb1eb29cd1c67537304e53
philhk <letsphil12@gmail.com>
2024-08-06 19:24:17 +0200

    feat: update remote urls

--- constants.go
+++ constants.go
@@ -11,9 +11,9 @@ import (
 	"vencordinstaller/buildinfo"
 )
 
-const ReleaseUrl = "https://api.github.com/repos/Vendicated/Vencord/releases/latest"
+const ReleaseUrl = "https://api.github.com/repos/philhk/Vencord/releases/latest"
 const ReleaseUrlFallback = "https://vencord.dev/releases/vencord"
-const InstallerReleaseUrl = "https://api.github.com/repos/Vencord/Installer/releases/latest"
+const InstallerReleaseUrl = "https://api.github.com/repos/philhk/Installer/releases/latest"
 const InstallerReleaseUrlFallback = "https://vencord.dev/releases/installer"
 
 var UserAgent = "VencordInstaller/" + buildinfo.InstallerGitHash + " (https://github.com/Vencord/Installer)"
